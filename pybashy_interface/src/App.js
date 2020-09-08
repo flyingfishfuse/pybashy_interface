7b290db72fb8f56ac2b4a6c53e01d892b36f3de2
flyingfishfusealt <46322912+flyingfishfuse@users.noreply.github.com>
2020-09-08 01:44:55 -0500

    adding electron

--- pybashy_interface/src/App.js
+++ pybashy_interface/src/App.js
@@ -48,6 +48,23 @@ const styles = theme => {
     };
   };
 
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
 const Player = withSounds()(props => (
     <button
         style={{ margin: 10 }}
